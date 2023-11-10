e8158ae3eabea6a259550192ceb95daca4a05ee4
mr-louback <loubacked4work@outlook.com>
2023-11-10 20:02:22 -0300

    fist commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <p>componente editado</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
